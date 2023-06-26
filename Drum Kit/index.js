6aa5c71156b9230d65265953785f2dd2c6b37153
Micaheal Johnston <36141218+md9j@users.noreply.github.com>
2023-06-26 17:00:21 -0600

    general event listener for drums made

---
+++ Drum Kit/index.js
@@ -0,0 +1,9 @@
+// alert("It's Aliiiive!");
+
+for(var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    
+    this.style.color = "white";
+}
+)}
